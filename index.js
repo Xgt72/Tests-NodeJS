f043650f9d272a16ba7512b72d61bbce0d442187
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-08-08 12:04:15 +0200

    solution for the test

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+const wilders = ["Loup", "Mickaël", "Thibault", "Richard"];
+console.log(chalk.blue(wilders[0]));
+console.log(chalk.red(wilders[1]));
+console.log(chalk.yellow(wilders[2]));
+console.log(chalk.green(wilders[3]));
